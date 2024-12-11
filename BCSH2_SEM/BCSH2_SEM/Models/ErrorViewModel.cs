b4ff1395298ab92ddf3c597a84b877de62c7ef6d
Petutu <st69646@upce.cz>
2024-12-11 15:41:38 +0100

    basics

---
+++ BCSH2_SEM/BCSH2_SEM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BCSH2_SEM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
